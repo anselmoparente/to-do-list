e9d2c8658dcfc9134b9237ac84ed29027fd7eeb3
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-10-20 14:11:54 -0300

    Created degisn dytem of this project

---
+++ lib/app/ui/theme/design_system.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ToDoColors {
+  ToDoColors._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color.fromARGB(255, 34, 32, 32);
+  static const Color backgroundColor = Color.fromARGB(255, 207, 245, 255);
+  static const Color buttonPrimary = Color.fromARGB(255, 54, 144, 141);
+}
+
+class ToDoTypography {
+  ToDoTypography._();
+}
+
+class ToDoIcons {
+  ToDoIcons._();
+}
